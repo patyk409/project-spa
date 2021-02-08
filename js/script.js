db89e32180849192e5ff1c504786f9cccce7422f
Dominik <68650553+patyk409@users.noreply.github.com>
2021-02-08 16:13:35 +0100

    project-spa

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.mobile-hamburger').addEventListener('click', function () {
+    document.querySelector('.open-menu-holder').classList.toggle('open');
+});
+
+document.querySelector('.mobile-closer').addEventListener('click', function () {
+    document.querySelector('.open-menu-holder').classList.toggle('open');
+});
